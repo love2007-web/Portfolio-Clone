7f3d623f21fefbd5e5234c7557a333e8b38a1c4b
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-10-10 10:34:34 +0100

    ..

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -5,7 +5,7 @@ import { Brush, Code, CreditCard2Back, Search } from "react-bootstrap-icons";
 const Middle = () => {
   return (
     <>
-      <div className="bg-[#0c1224] mt-[-20px] p-4 shadow-2xl grid grid-cols-2 lg:grid-cols-4 justify-around mx-auto lg:w-[1200px]">
+      <div className="bg-[#0c1224] font-montserrat mt-[-20px] p-4 shadow-2xl grid grid-cols-2 lg:grid-cols-4 justify-around mx-auto lg:w-[1200px]">
         <div className="text-white w-auto p-4">
           <div className="flex justify-center">
             <Brush className="text-white text-3xl" />
--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -3,11 +3,50 @@ import React from 'react'
 const Next = () => {
   return (
     <>
+      <div className="font-montserrat mt-10 flex px-[75px]">
         <div>
-            
+          <div className="w-96 font-black">
+            <h1 className="text-3xl">Make your business Standout</h1>
+            <p className="text-md mt-5">in a sea of sameness...</p>
+          </div>
+          <p className="font-black my-7">__________</p>
+          <div className="w-[450px] text-sm">
+            <p>
+              Are you a business owner, online course creator, boutique agency,
+              or personal brand?
+            </p>
+            <p className="mt-3">
+              Do you want a website that professionally communicates your brand
+              to your target audience, one that is well-functional, responsive,
+              and easy to manage?
+            </p>
+            <p className="mt-3">
+              Or, do you need to create a new online presence using a
+              world-class web design that is optimized for SEO but you don't
+              have the time to build your website and you're busy working on
+              your business…
+            </p>
+          </div>
         </div>
+        <div className="bg-red-600 ml-4">
+          <div>
+            <img
+              src="https://kraftle.com/wp-content/uploads/2022/01/Home-Ultimate-Viewnew-cover-300x300.webp"
+              alt=""
+              className="shadow-2xl"
+            />
+            <div className='w-40 h-40'>
+              <img
+                src="https://images.pexels.com/photos/699459/pexels-photo-699459.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+                className='w-full h-full'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Next
