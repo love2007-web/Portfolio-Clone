6d2d748ca58e0ad61e16e2ca497268622e0a96c8
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-10-16 12:57:07 +0100

    done moblie

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { EnvelopeFill } from "react-bootstrap-icons";
+import { Linkedin } from "react-bootstrap-icons";
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <div>
+        <div className="bg-[#091224] h-[50px] flex px-7 justify-between text-white">
+          <div>
+            <h2 className="font-montez text-xl md:text-2xl lg:text-3xl mt-0 font-bold">
+              Creaftech.
+            </h2>
+            <p className="text-[5px] font-montserrat mt-2 mb-[-10px] font-bold">
+              SEO-Savvy Web Designer in Lagos
+            </p>
+          </div>
+          <div className="flex">
+            <button className="mr-1 ">
+              <EnvelopeFill
+                size={20}
+                className="transition  ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-110 hover:text-[#4F3D65] duration-300"
+              />
+            </button>
+            <button className="mr-1 ">
+              <Linkedin
+                size={20}
+                className="transition ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-110 hover:text-[#4F3D65] duration-300"
+              />
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-between p-7 text-white bg-black">
+          <div className='text-xs'>
+            <p>Kraftle ©2022 All Rights Reserved.</p>
+          </div>
+          <div className='text-xs'>
+            <p>Privacy Policy</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Footer
